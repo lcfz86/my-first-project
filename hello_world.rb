8101312ab3ecc1a4ec18083d0ea221ec92ed8937
lcfz86 <zerolcfz86@gmail.com>
2021-06-04 19:55:07 +0800

    My first exersice project

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,16 @@
 puts('Hello World!')
+
+puts('Please enter your name: ')
+name = gets.chomp
+
+puts('Please enter your birth year')
+birth_year = gets.chomp.to_i
+
+puts('Please enter your birth place')
+birth_place = gets.chomp
+
+birth_year = 2021 - birth_year
+
+puts("Hi, my name is #{name}, i was born in #{birth_place}")
+puts("in year 2021, I am #{birth_year} years old.")
+puts('Today is my first day of class at Rambutan Code Academy.')
